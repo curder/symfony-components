d58484dcba720e71c45a32c9bff567cc0ffc67f8
curder <q.curder@gmail.com>
2022-01-15 23:46:09 +0800

    Add namespace

--- src/CreateUserCommand.php
+++ src/CreateUserCommand.php
@@ -4,6 +4,7 @@ namespace Curder;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,11 +17,13 @@ class CreateUserCommand extends Command
              ->setDescription('Create new user') // 命令简短描述
              ->setHelp('This command allows you to create users...') // 运行命令时使用 "--help" 选项时的完整命令描述
              ->addArgument('username', InputArgument::REQUIRED, 'The username of the user.')
-//             ->addOption('is_admin', InputArgument::OPTIONAL, InputOption::VALUE_NONE, 'It\'s admin user.')
-//             ->setDefinition([
-//                 new InputArgument('username', InputArgument::REQUIRED, 'The username of the user.'),
-//                 new InputOption('is_admin', InputArgument::OPTIONAL, InputOption::VALUE_NONE, 'It\'s admin user.'),
-//             ])
+//             ->addOption('is_admin', 'a', InputArgument::OPTIONAL, 'It\'s admin user.')
+//             ->setDefinition(
+//                 new InputDefinition([
+//                     new InputArgument('username', InputArgument::REQUIRED, 'The username of the user.'),
+//                     new InputOption('is_admin', 'a', InputArgument::OPTIONAL, 'It\'s admin user.'),
+//                ])
+//             )
         ;
     }
 
