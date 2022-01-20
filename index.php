0b2f9d635a0c765c330f12bdd0936d56c870b09f
curder <q.curder@gmail.com>
2022-01-20 14:45:31 +0800

    Add some dump demo

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+use Curder\User;
+
+require __DIR__.'/vendor/autoload.php';
+
+$user = new User;
+
+dd($user);
---
+++ src/User.php
@@ -0,0 +1,13 @@
+<?php
+namespace Curder;
+
+class User
+{
+    public string $username = 'Curder';
+
+    protected int $age = 30;
+
+    private array $hobbies = [
+        "music", 'coding',
+    ];
+}
